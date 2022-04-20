a18fd01ccbcb05a8db83fef97779542b58295a4e
Jolene Kearse <70668540+JoleneKearse@users.noreply.github.com>
2022-04-20 19:04:14 -0400

    Added according menu & intro
    Styled according menu, added new font, added intro

--- index.js
+++ index.js
@@ -1,60 +1,12 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const tabBtn = document.getElementById("tab-btn")
-const deleteBtn = document.getElementById("delete-btn")
-const ulEl = document.getElementById("ul-el")
+const accordian = document.querySelectorAll(".accordian")
 
-// get leads from localStorage, turn back into an arr, save in new variable
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
-// only display leads if there are any saved in storage
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-inputBtn.addEventListener("click", function() {
-    // add new leads to arr
-    myLeads.push(inputEl.value)
-    // clear the input field
-    inputEl.value = ""
-    // save leads to localStorage as str
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
-})
-
-// grab the current tab from Google API
-tabBtn.addEventListener("click", function() {
-    // access both the chrome object & tabs object
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    })
-})
-
-// delete leads on double click
-deleteBtn.addEventListener("dblclick", function() {
-    // clear storage, myLeads arr, ulEl
-    localStorage.clear()
-    myLeads = []
-    ulEl.innerHTML = ""
-})
-
-function render(leads) {
-    // empty str to hold HTML for list items
-    let listItems = ""
-    // loop through leads to add to unordered list
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `
-    }
-    // access DOM to display on page
-    ulEl.innerHTML = listItems  
+for (let i = 0; i < accordian.length; i++) {
+    accordian[i].addEventListener("click", function() {
+        let content = this.nextElementSibling
+        if (content.classList.contains("active")) {
+            content.classList.remove("active")
+        } else {
+            content.classList.add("active")
+        }
+    }) 
 }
